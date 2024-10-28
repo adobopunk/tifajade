2daadd06c272b0f81f29d45b2d296bd3b4dcc443
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-10-28 14:23:07 -0700

    added Front Matter plugin but not set up yet. Repaired home page layout and video width bug

--- _site/app/js/showreel.js
+++ _site/app/js/showreel.js
@@ -2,38 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
   const showreelButton = document.querySelector(".showreel");
   const overlay = document.getElementById("showreel-overlay");
   const closeButton = document.querySelector(".close-overlay");
-  const iframe = document.querySelector(".vimeo-container iframe"); // Select the iframe
-  const player = new Vimeo.Player(iframe); // Initialize the Vimeo player
+  const iframe = document.querySelector(
+    "#showreel-overlay .vimeo-container iframe"
+  );
+  const player = new Vimeo.Player(iframe); // Initialize Vimeo player for this iframe
 
-  // Show overlay when button is clicked
+  // Show overlay and play video when button is clicked
   showreelButton.addEventListener("click", function () {
     overlay.classList.add("active");
-    overlay.style.display = "flex"; // Set to flex when active to enable flex styles
+    overlay.style.display = "flex";
     setTimeout(() => {
-      overlay.style.opacity = "1"; // Set opacity after display
-    }, 10); // Small timeout to ensure display is applied before opacity
-    player.play(); // Start playing the video
+      overlay.style.opacity = "1";
+      player.play(); // Start playing the video on overlay open
+    }, 10);
   });
 
-  // Hide overlay when close button is clicked
+  // Hide overlay, reset video time, and stop playback when close button is clicked
   closeButton.addEventListener("click", function () {
     overlay.style.opacity = "0"; // Fade out effect
     overlay.addEventListener(
       "transitionend",
       function () {
-        overlay.classList.remove("active"); // Remove active class after fade out
-        overlay.style.display = "none"; // Set back to none
+        overlay.classList.remove("active");
+        overlay.style.display = "none";
         player
-          .setCurrentTime(0) // Reset video playback position to the beginning
-          .then(function () {
-            // Optionally, you can pause it after resetting the time
-            player.pause();
-          })
+          .setCurrentTime(0) // Reset video to start
+          .then(() => player.pause()) // Stop playback after resetting time
           .catch(function (error) {
-            console.error("Error setting playback time:", error);
+            console.error("Error stopping playback:", error);
           });
       },
       { once: true }
-    ); // Remove listener after it runs
+    ); // Listener removed after running once
   });
 });
---
+++ src/projects/2024-10-28-frog-bones.md
@@ -0,0 +1,10 @@
+---
+title: Frog Bones
+description: ""
+date: 2024-10-28T20:00:39.613Z
+preview: ""
+draft: true
+tags: []
+categories: []
+type: default
+---
