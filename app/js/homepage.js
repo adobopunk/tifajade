989f20850442228f2604085578433be47315cbc8
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-11-02 14:30:23 -0700

    updated reel and hero bg

--- app/js/homepage.js
+++ app/js/homepage.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Handle video background
-  const videoBackground = document.querySelector(".video-background");
-  const iframe = videoBackground.querySelector("iframe");
-
-  // Initially set the iframe src to an empty URL to avoid grey box
-  iframe.src = "about:blank";
-
-  // Set the src of the iframe for the background video after the page has loaded
-  window.addEventListener("load", function () {
-    // Set the src of the iframe
-    iframe.src =
-      "https://player.vimeo.com/video/544297893?autoplay=1&loop=1&background=1&enablejsapi=1";
-
-    // Delay before fading in the iframe
-    setTimeout(() => {
-      iframe.style.opacity = "1"; // Fade in effect
-    }, 1200); // Adjust delay time as needed (1200ms = 1.2 seconds)
-  });
-
   // Handle showreel button
   const showreelButton = document.getElementById("showreelButton");
 
@@ -25,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   if (showreelButton) {
     // Add a delay before showing the button
     setTimeout(() => {
-      showreelButton.classList.add("show"); // Add the 'show' class after 1 second
-    }, 800); // 1000ms delay
+      showreelButton.classList.add("show"); // Add the 'show' class after 800ms
+    }, 800); // 800ms delay
   } else {
     console.error("Showreel button not found!"); // Debugging message if button is not found
   }
