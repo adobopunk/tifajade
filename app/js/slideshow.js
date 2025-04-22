0503f48f8c8d17cd7cb8522e6f3bb0fa5609707b
TJ Lien <89636647+tjlien@users.noreply.github.com>
2025-04-21 20:51:22 -0700

    update about page to slideshow

--- app/js/slideshow.js
+++ app/js/slideshow.js
@@ -6,35 +6,35 @@ const pages = [
   },
   {
     copy: "I grew up in Richland, Washington, making backyard movies with friends. I loved action films like The Matrix and Mission: Impossible. I taught myself After Effects in 5th grade, entered student art competitions and earned a spot in independent study programs to create original projects and sharpen my filmmaking skills long before high school.",
-    src: "/assets/img/videos/AboutLoop2_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop2_h264_preview.mp4",
   },
   {
     copy: "I studied film at Eastern Washington University, where four of my short films screened at the Spokane International Film Festival and I was named “Most Promising Filmmaker.” I earned multiple scholarships, but most importantly, I learned how to tell meaningful stories, lead a great crew, and direct successful projects end-to-end. By the time I graduated, I knew I wanted to keep telling stories for a living, and I carried those skills into every role since.",
-    src: "/assets/img/videos/AboutLoop3_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop3_h264_preview.mp4",
   },
   {
     copy: "My creative path led me to my first leadership role as Creative Services Director at an NBC affiliate, where the pay was terrible and the experience was invaluable. I pitched, directed, edited, and designed campaigns while rapidly developing my motion graphics skills. I built strong partnerships with local business owners and produced the station’s first Spanish-language newscast for Telemundo — a milestone in community outreach for the area.",
-    src: "/assets/img/videos/AboutLoop4_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop4_h264_preview.mp4",
   },
   {
     copy: "Eager to push beyond small-town markets, I pitched a docuseries about a rising high school football team to my boss, and convinced him to say yes. I filmed what became a championship season, edited down thousands of hours of footage, and delivered the entire project to air on TV as a solo director/producer, barely a year after graduating college. That experience opened the door to an unforgettable opportunity in Las Vegas.",
-    src: "/assets/img/videos/AboutLoop5_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop5_h264_preview.mp4",
   },
   {
     copy: "In 2018, I relocated to Las Vegas to join an esports startup called Impact Gaming. I shaped their brand identity and content strategy, producing another docuseries. I wasn't an expert in esports—nor had I been any sort of expert in football. But I knew how to build trust, capture honest stories, and craft emotionally resonant narratives that connect with audiences.",
-    src: "/assets/img/videos/AboutLoop6_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop6_h264_preview.mp4",
   },
   {
     copy: "My storytelling portfolio and motion design work opened the door to UFC’s original content department, where I spent four years producing branded content, editing social media and long-form programming, and marketing record-breaking sports events. I thrived in one of the world’s most intense, fast-paced creative environments — sharpening my creative instincts and leadership skills.",
-    src: "/assets/img/videos/AboutLoop7_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop7_h264_preview.mp4",
   },
   {
     copy: "I reconnected my love of storytelling with my passion for video games when I was asked to lead BoomTV's video production team. Directing creative projects at a games marketing agency meant partnering with names like Fortnite, Halo, and Call of Duty, designing events that earned millions of impressions, and recognition from industry veterans at the highest levels of game marketing.",
-    src: "/assets/img/videos/AboutLoop9_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop9_h264_preview.mp4",
   },
   {
     copy: "Across massive global brands, fast-growing startups, and scrappy creative teams, I’ve applied storytelling, strategy, and production leadership to solve numerous creative challenges. No matter what the mission is, I'm there because I love the craft — bringing bold ideas to life, building workflows that dismantle creative roadblocks, and pushing the boundaries of visual storytelling.",
-    src: "/assets/img/videos/AboutLoop8_h264_crf23.mp4",
+    src: "/assets/img/videos/AboutLoop8_h264_preview.mp4",
   },
   {
     copy: "Today, I’m a freelance Creative Director running my agency, Tiny Pond, and producing events for the games industry through our venture In Queue. If you’re building bold, high-impact projects and need a creative leader who thrives under pressure and lives for ambitious, slightly crazy ideas — let’s connect. You might just be my Slide 11.",
