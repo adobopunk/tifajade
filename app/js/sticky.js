fde9c0d65648eec6ff2a8abb59ec9134c9f0c275
TJ Lien <89636647+tjlien@users.noreply.github.com>
2025-04-24 13:34:50 -0700

    optimization fixes

--- app/js/sticky.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  const stickyNavbar = document.querySelector(".sticky-navbar");
-  const footer = document.querySelector(".footer");
-
-  const footerHeight = footer.offsetHeight;
-  stickyNavbar.style.marginBottom = `${footerHeight}px`;
-});
